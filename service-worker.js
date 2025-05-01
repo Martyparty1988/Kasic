4bfba4cc7530e735965d3ac91b295f879f73583a
Marty <rosary-vassals8m@icloud.com>
2025-05-01 09:11:50 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+// Název cache
+const CACHE_NAME = 'kasa-app-v1';
+
+// Soubory pro cache
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/static/js/main.chunk.js',
+  '/static/js/0.chunk.js',
+  '/static/js/bundle.js',
+  '/manifest.json',
+  '/favicon.ico',
+  '/logo192.png',
+  '/logo512.png'
+];
+
+// Instalace Service Workeru
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Aktivace Service Workeru
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          return cacheName !== CACHE_NAME;
+        }).map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+// Fetch události
